ae0f0153910aa1c82fc230636b9ef78869c93177
Danielle Souza <dany.sa1@hotmail.com>
2025-06-24 08:11:39 -0300

    feat: add login logic

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,48 @@
-import React from "react-native";
+import React, { Alert, Keyboard } from "react-native";
 import * as S from "./styles";
+import { useState, useRef } from "react";
+import { signIn } from "../../services/login";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  const onLogin = async () => {
+    Keyboard.dismiss();
+    try {
+      const { email, password } = user;
+      const { token } = await signIn({ email, password });
+      if (token) {
+        navigation.navigate("Eventos");
+      }
+    } catch (error) {
+      Alert.alert("Erro ao logar. Por favor confirme seu login e senha!");
+    }
+  };
+
   return (
     <S.PageContent>
       <S.Title>Faça seu login 🔑</S.Title>
-     <S.Label>E-mail ou usuário</S.Label>
-      <S.InputComponent  placeholder="marcos.oliveira@agendae" >
-      </S.InputComponent>
-       <S.Label margin>Senha</S.Label>
-      <S.InputComponent  placeholder="*********" >
-      </S.InputComponent>
-      <S.ButtonComponent onPress={()=> console.log('teste')}>
+      <S.Label>E-mail ou usuário</S.Label>
+      <S.InputComponent
+        placeholder="marcos.oliveira@agendae"
+        value={user.email}
+        onChangeText={(value) => setUser({ ...user, email: value })}
+      ></S.InputComponent>
+      <S.Label margin>Senha</S.Label>
+      <S.InputComponent
+        placeholder="******"
+        value={user.password}
+        onChangeText={(value) => setUser({ ...user, password: value })}
+        onSubmitEditing={onLogin}
+      ></S.InputComponent>
+      <S.ButtonComponent onPress={onLogin}>
         <S.LabelButton>Entrar</S.LabelButton>
       </S.ButtonComponent>
     </S.PageContent>
---
+++ src/services/login.ts
@@ -0,0 +1,19 @@
+type Request = {
+  email: string;
+  password: string;
+};
+export const signIn = async ({
+  email,
+  password,
+}: Request): Promise<{ token: string }> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (email !== "student@ae.com" && password != "123456") {
+        return reject(new Error("Usuário inválido"));
+      }
+      resolve({
+        token: "ABC",
+      });
+    }, 3000);
+  });
+};
