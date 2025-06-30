23c1b7d14d67e7fe3063bae55aa9c08e74f781ec
Danielle Souza <dany.sa1@hotmail.com>
2025-06-30 13:42:21 -0300

    feat: adjust routes

--- src/routes /index.tsx
+++ src/routes /index.tsx
@@ -1,20 +1,53 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from '../pages/Login/Login';
-import Home from '../pages/Home/Home';
-import Details from '../pages/Details.tsx/Details';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../pages/Login/Login";
+import Home from "../pages/Home/Home";
+import Details from "../pages/Details.tsx/Details";
+import { theme } from "../styles/theme";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="events">
-        <Stack.Screen name="login" options={{ headerShown: false }} component={Login} />
-         <Stack.Screen name="events" options={{ title: 'Eventos', headerBackVisible: false}}  component={Home} />
-         <Stack.Screen name="details" options={{ title: 'Detalhes do evento', headerBackTitle:'', }}  component={Details} />
+      <Stack.Navigator
+        initialRouteName="events"
+        screenOptions={{
+          headerTitleAlign: "left",
+        }}
+      >
+        <Stack.Screen
+          name="login"
+          options={{ headerShown: false }}
+          component={Login}
+        />
+        <Stack.Screen
+          name="events"
+          options={{ title: "Eventos" }}
+          component={Home}
+        />
+        <Stack.Screen
+          name="details"
+          options={({ navigation }) => ({
+            title: "Detalhes do evento",
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                style={{ marginLeft: 10, marginRight: 10 }}
+              >
+                <Ionicons
+                  name="arrow-back"
+                  size={24}
+                  color={theme.colors.default}
+                />
+              </TouchableOpacity>
+            ),
+          })}
+          component={Details}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
